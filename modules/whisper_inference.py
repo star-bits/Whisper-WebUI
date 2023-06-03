f869e2a1c03d7a464f3c14566e4efc693a95e895
star-bits <star-bits@outlook.com>
2023-06-03 15:19:58 +0900

    forked from jhj0517/Whisper-WebUI

---
+++ modules/whisper_inference.py
@@ -0,0 +1,118 @@
+import whisper
+from modules.subtitle_manager import get_srt, get_vtt, write_file, safe_filename
+from modules.youtube_manager import get_ytdata, get_ytaudio
+import gradio as gr
+import os
+from datetime import datetime
+
+DEFAULT_MODEL_SIZE = "large-v2"
+
+
+class WhisperInference:
+    def __init__(self):
+        self.current_model_size = None
+        self.model = None
+        self.available_models = whisper.available_models()
+        self.available_langs = sorted(list(whisper.tokenizer.LANGUAGES.values()))
+
+    def transcribe_file(self, fileobjs,
+                        model_size, lang, subformat, istranslate,
+                        progress=gr.Progress()):
+
+        try:
+            if model_size != self.current_model_size or self.model is None:
+                progress(0, desc="Initializing Model..")
+                self.current_model_size = model_size
+                self.model = whisper.load_model(name=model_size, download_root="models/Whisper")
+
+            if lang == "Automatic Detection":
+                lang = None
+
+            progress(0, desc="Loading Audio..")
+
+            files_info = {}
+            for fileobj in fileobjs:
+
+                audio = whisper.load_audio(fileobj.name)
+
+                translatable_model = ["large", "large-v1", "large-v2"]
+                if istranslate and self.current_model_size in translatable_model:
+                    result = self.model.transcribe(audio=audio, language=lang, verbose=False, task="translate")
+                else:
+                    result = self.model.transcribe(audio=audio, language=lang, verbose=False)
+
+                progress(1, desc="Completed!")
+
+                file_name, file_ext = os.path.splitext(os.path.basename(fileobj.orig_name))
+                file_name = file_name[:-9]
+                file_name = safe_filename(file_name)
+                timestamp = datetime.now().strftime("%m%d%H%M%S")
+                output_path = f"outputs/{file_name}-{timestamp}"
+
+                if subformat == "SRT":
+                    subtitle = get_srt(result["segments"])
+                    write_file(subtitle, f"{output_path}.srt")
+                elif subformat == "WebVTT":
+                    subtitle = get_vtt(result["segments"])
+                    write_file(subtitle, f"{output_path}.vtt")
+
+                files_info[file_name] = subtitle
+
+            total_result = ''
+            for file_name, subtitle in files_info.items():
+                total_result += '------------------------------------\n'
+                total_result += f'{file_name}\n\n'
+                total_result += f'{subtitle}'
+
+            return f"Done! Subtitle is in the outputs folder.\n\n{total_result}"
+        except Exception as e:
+            return str(e)
+        finally:
+            for fileobj in fileobjs:
+                if os.path.exists(fileobj.name):
+                    os.remove(fileobj.name)
+
+    def transcribe_youtube(self, youtubelink,
+                           model_size, lang, subformat, istranslate,
+                           progress=gr.Progress()):
+
+        try:
+            if model_size != self.current_model_size or self.model is None:
+                progress(0, desc="Initializing Model..")
+                self.current_model_size = model_size
+                self.model = whisper.load_model(name=model_size, download_root="models/Whisper")
+
+            if lang == "Automatic Detection":
+                lang = None
+
+            progress(0, desc="Loading Audio from Youtube..")
+            yt = get_ytdata(youtubelink)
+            audio = whisper.load_audio(get_ytaudio(yt))
+
+            translatable_model = ["large", "large-v1", "large-v2"]
+            if istranslate and self.current_model_size in translatable_model:
+                result = self.model.transcribe(audio=audio, language=lang, verbose=False, task="translate")
+            else:
+                result = self.model.transcribe(audio=audio, language=lang, verbose=False)
+
+            progress(1, desc="Completed!")
+
+            file_name = safe_filename(yt.title)
+            timestamp = datetime.now().strftime("%m%d%H%M%S")
+            output_path = f"outputs/{file_name}-{timestamp}"
+
+            if subformat == "SRT":
+                subtitle = get_srt(result["segments"])
+                write_file(subtitle, f"{output_path}.srt")
+            elif subformat == "WebVTT":
+                subtitle = get_vtt(result["segments"])
+                write_file(subtitle, f"{output_path}.vtt")
+
+            return f"Done! Subtitle file is in the outputs folder.\n\n{subtitle}"
+        except Exception as e:
+            return str(e)
+        finally:
+            yt = get_ytdata(youtubelink)
+            file_path = get_ytaudio(yt)
+            if os.path.exists(file_path):
+                os.remove(file_path)
---
+++ ui/htmls.py
@@ -0,0 +1,38 @@
+CSS = """
+.bmc-button {
+    padding: 2px 5px;
+    border-radius: 5px;
+    background-color: #FF813F;
+    color: white;
+    box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.3);
+    text-decoration: none;
+    display: inline-block;
+    font-size: 20px;
+    margin: 2px;
+    cursor: pointer;
+    -webkit-transition: background-color 0.3s ease;
+    -ms-transition: background-color 0.3s ease;
+    transition: background-color 0.3s ease;
+}
+.bmc-button:hover,
+.bmc-button:active,
+.bmc-button:focus {
+    background-color: #FF5633;
+}
+.markdown {
+    margin-bottom: 0;
+    padding-bottom: 0;
+}
+.tabs {
+    margin-top: 0;
+    padding-top: 0;
+}
+
+#md_project a {
+  color: black;
+  text-decoration: none;
+}
+#md_project a:hover {
+  text-decoration: underline;
+}
+"""
